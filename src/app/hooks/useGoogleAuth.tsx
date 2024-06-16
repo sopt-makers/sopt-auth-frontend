1999bae7aee8c96c2eae017799365ee108aa5461
Brokyeom <goindol40@gmail.com>
2024-06-16 16:54:04 +0900

    feat : 구글로그인 인가코드 로직 연결

---
+++ src/app/hooks/useGoogleAuth.tsx
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+const GOOGLE_LOGIN_CALLBACK_URI = `${process.env.NEXT_PUBLIC_ORIGIN}/auth/callback/google/login`;
+const GOOGLE_REGISTER_CALLBACK_URI = `${process.env.NEXT_PUBLIC_ORIGIN}/auth/callback/google/register`;
+
+interface GoogleAuth {
+  login(): void;
+  register(): void;
+//   sendLoginRequest(
+//     code: string,
+//     state: string,
+//   ): Promise<
+//     { success: true; accessToken: string } | { success: false; error: 'invalidNonce' | 'notMember' | 'unknown' }
+//   >;
+//   sendRegisterRequest(
+//     code: string,
+//     registerToken: string,
+//     state: string,
+//   ): Promise<{ success: true; accessToken: string } | { success: false }>;
+//   sendResetRequest(
+//     code: string,
+//     registerToken: string,
+//     state: string,
+//   ): Promise<{ success: true; accessToken: string } | { success: false }>;
+  isAvailable: boolean;
+}
+
+const useGoogleAuth = (): GoogleAuth => {
+
+
+  return {
+    isAvailable: process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID !== '',
+    login() {
+      open(
+        `https://accounts.google.com/o/oauth2/v2/auth?scope=openid&response_type=code&redirect_uri=${encodeURIComponent(
+          GOOGLE_LOGIN_CALLBACK_URI,
+        )}&client_id=${process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID}`,
+        '_parent',
+      );
+    },
+    register() {
+      open(
+        `https://accounts.google.com/o/oauth2/v2/auth?scope=openid&response_type=code&redirect_uri=${encodeURIComponent(
+          GOOGLE_REGISTER_CALLBACK_URI,
+        )}&client_id=${process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID}`,
+        '_parent',
+      );
+    },
+  };
+};
+
+export default useGoogleAuth;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 'use client';
 
+import useGoogleAuth from "./hooks/useGoogleAuth";
+
+
+
 const page = () => {
+const googleLoginAuth = useGoogleAuth();
+
   return (
-    <button>구글로 로그인하기</button>
+    <button onClick={googleLoginAuth.login}>구글로 로그인하기</button>
   )
 }
 
