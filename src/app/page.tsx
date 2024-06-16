9711c11b43c896576c50d4b7cd9422d9c67075a3
Brokyeom <goindol40@gmail.com>
2024-06-16 17:29:51 +0900

    feat : 애플 로그인 로직 이식

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 'use client';
 
+import useAppleAuth from "./hooks/useAppleAuth";
 import useGoogleAuth from "./hooks/useGoogleAuth";
 
 
 
 const page = () => {
 const googleLoginAuth = useGoogleAuth();
+const appleLoginAuth = useAppleAuth();
 
   return (
+    <>
     <button onClick={googleLoginAuth.login}>구글로 로그인하기</button>
+    <button onClick={appleLoginAuth.login}>애플로 로그인하기</button>
+    </>
   )
 }
 
