a87684ef1c1c814329ca9f1a025f3977fb37ec79
Brokyeom <goindol40@gmail.com>
2024-06-18 22:57:01 +0900

    chore : axios instance 생성

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,1 +1,10 @@
+import axios from 'axios';
+
 const API_URL = 'https://operation.api.dev.sopt.org/api/v1';
+
+const instanceConfig = {
+  baseURL: API_URL,
+  headers: { 'Content-Type': 'application/json' },
+};
+
+export const axiosInstance = axios.create(instanceConfig);
