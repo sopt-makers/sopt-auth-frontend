b56b0e821d7423beb20ad5a8dfc1a99c84b96bf8
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-01-30 17:45:49 +0900

    feat: add logic to LastLoginBanner.tsx

--- src/components/main/LastLoggedInBanner.tsx
+++ src/components/main/LastLoggedInBanner.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { css } from "@/styled-system/css";
 
 function LastLoggedInBanner() {
   const [lastLoggedInAccount, setLastLoggedInAccount] = useState<
     "apple" | "google" | null
   >("google"); // 마지막으로 로그인한 계정
+
+  useEffect(() => {
+    const lastLoggedInAccount = localStorage.getItem("sopt-lastRegister");
+    if (lastLoggedInAccount === "google" || lastLoggedInAccount === "apple") {
+      setLastLoggedInAccount(lastLoggedInAccount);
+    }
+  }, []);
+
   return (
     <div className={css({ ...lastLoggedInBannerStyles })}>
       {lastLoggedInAccount && (
