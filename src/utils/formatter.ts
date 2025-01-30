21205fd920fbaec2456486193a1d2e54013289d9
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-01-30 17:43:16 +0900

    feat: add google util functions and link google login url

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,4 +1,4 @@
-export const formatTime = (time: number) => {
+export const formatTime = (time: number): string => {
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
   return `${minutes}:${seconds.toString().padStart(2, "0")}`;
