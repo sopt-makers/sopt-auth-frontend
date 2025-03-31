57f65a42971a027c1865cfdc3d85a55a15184fc9
10tentacion <ddww1029wwdd@gmail.com>
2025-04-01 01:57:28 +0900

    feat: add api hooks in redirect page

--- src/routes/auth/google/callback/index.tsx
+++ src/routes/auth/google/callback/index.tsx
@@ -1,45 +1,71 @@
-import { useEffect } from "react";
-import { useLogin } from "@/src/hooks/useLogin";
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { jwtDecode } from "jwt-decode";
-import { validateNonce } from "@/src/utils/nonce";
-
-interface GoogleCallbackSearch {
-  token: string;
-}
-
-export const Route = createFileRoute("/auth/google/callback/")({
-  validateSearch: (search: Record<string, unknown>): GoogleCallbackSearch => {
-    return {
-      token: search.token as string,
-    };
-  },
+import { useEffect } from 'react';
+import { useLogin } from '@/src/hooks/useLogin';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+import { jwtDecode } from 'jwt-decode';
+import { validateNonce } from '@/src/utils/nonce';
+import { useSignUp } from '@/src/hooks/useSignUp';
+import { useUpdateSocialAccount } from '@/src/hooks/useUpdateSocialAccount';
+export const Route = createFileRoute('/auth/google/callback/')({
   component: Index,
 });
 
 function Index() {
   const navigate = useNavigate();
   const { handleLogin } = useLogin();
+  const { handleSignUp } = useSignUp();
+  const { handleUpdateSocialAccount } = useUpdateSocialAccount();
 
   useEffect(() => {
     const hash = window.location.hash.substring(1);
     const params = new URLSearchParams(hash);
-    const token = params.get("id_token");
+    const token = params.get('id_token');
+    const state = params.get('state');
 
     if (!token) {
-      alert("잘못된 접근입니다. 다시 시도해주세요");
-      navigate({ to: "/" });
+      console.log('token 없음');
+      alert('잘못된 접근입니다. 다시 시도해주세요');
+      navigate({ to: '/' });
     } else {
       const decoded = jwtDecode(token);
 
       if (!validateNonce(decoded)) {
-        alert("토큰이 유효하지 않습니다. 다시 로그인 해주세요");
-        navigate({ to: "/" });
+        alert('토큰이 유효하지 않습니다. 다시 로그인 해주세요');
+        navigate({ to: '/', replace: true });
       }
 
-      handleLogin({ token, authPlatform: "GOOGLE" });
+      switch (state) {
+        case 'login':
+          handleLogin({ token, authPlatform: 'GOOGLE' });
+          break;
+        case 'update':
+          {
+            const phone = sessionStorage.getItem('phone');
+            if (!phone) {
+              alert('잘못된 접근입니다. 다시 시도해주세요');
+              navigate({ to: '/social-account-linking/auth', replace: true });
+            } else {
+              handleUpdateSocialAccount({ token, authPlatform: 'GOOGLE', phone });
+            }
+          }
+          break;
+        case 'signup':
+          {
+            const name = sessionStorage.getItem('name');
+            const phone = sessionStorage.getItem('phone');
+            if (!name || !phone) {
+              alert('잘못된 접근입니다. 다시 시도해주세요');
+              navigate({ to: '/sign-up/auth', replace: true });
+            } else {
+              handleSignUp({ token, authPlatform: 'GOOGLE', name, phone });
+            }
+          }
+          break;
+        default:
+          alert('잘못된 접근입니다. 다시 시도해주세요');
+          navigate({ to: '/', replace: true });
+      }
     }
-  }, [navigate, handleLogin]);
+  }, []);
 
   return <></>;
 }
