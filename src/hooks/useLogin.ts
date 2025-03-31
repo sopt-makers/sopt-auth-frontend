e21775b953d116d63116df44867fb58d1d04fe8d
10tentacion <ddww1029wwdd@gmail.com>
2025-04-01 01:47:13 +0900

    feat: update navigation path in useLogin hook

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,5 +1,5 @@
-import { postLogin, PostLoginRequest } from "@/src/api/postLogin";
-import { useNavigate } from "@tanstack/react-router";
+import { postLogin, PostLoginRequest } from '@/src/api/postLogin';
+import { useNavigate } from '@tanstack/react-router';
 
 export const useLogin = () => {
   const navigate = useNavigate();
@@ -7,18 +7,16 @@ export const useLogin = () => {
   const handleLogin = async ({ token, authPlatform }: PostLoginRequest) => {
     try {
       const response = await postLogin({ token, authPlatform });
-      localStorage.setItem("serviceAccessToken", response.data.accessToken);
+      localStorage.setItem('serviceAccessToken', response.data.accessToken);
       // NOTE: 기존 pg-lastRegister를 sopt-lastRegister로 변경
-      localStorage.setItem(
-        "sopt-lastRegister",
-        authPlatform === "GOOGLE" ? "google" : "apple"
-      );
+      localStorage.setItem('sopt-lastRegister', authPlatform === 'GOOGLE' ? 'google' : 'apple');
+      window.location.href = 'https://playground.sopt.org/intro';
     } catch (error) {
       if (error instanceof Error) {
         alert(error.message);
+        navigate({ to: '/', replace: true });
       }
     }
-    navigate({ to: "/" });
   };
 
   return { handleLogin };
