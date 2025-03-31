20c98b02aa852a6f2e0379cef2a414b101a21d13
10tentacion <ddww1029wwdd@gmail.com>
2025-04-01 02:26:05 +0900

    feat: modify timer time limit

--- src/components/common/AuthSection.tsx
+++ src/components/common/AuthSection.tsx
@@ -1,27 +1,23 @@
-"use client";
-
-import { ReactNode, ChangeEvent, useState } from "react";
-import { useNavigate } from "@tanstack/react-router";
-import { Button, TextField } from "@sopt-makers/ui";
-import { css, cx } from "@/styled-system/css";
-import { useTimer } from "@/src/hooks/useTimer";
-import { formatTime } from "@/src/utils/formatter";
-import { postAuthPhone } from "@/src/api/postAuthPhone";
-import { postVerifyPhone } from "@/src/api/postVerifyPhone";
+import { ReactNode, ChangeEvent, useState } from 'react';
+import { useNavigate } from '@tanstack/react-router';
+import { Button, TextField } from '@sopt-makers/ui';
+import { css, cx } from '@/styled-system/css';
+import { useTimer } from '@/src/hooks/useTimer';
+import { formatTime } from '@/src/utils/formatter';
+import { postAuthPhone } from '@/src/api/postAuthPhone';
+import { postVerifyPhone } from '@/src/api/postVerifyPhone';
 
 interface AuthSectionProps {
   children?: ReactNode;
-  nextURL: "/sign-up/social" | "/social-account-linking/social";
+  nextURL: '/sign-up/social' | '/social-account-linking/social';
 }
 
 function AuthSection({ children, nextURL }: AuthSectionProps) {
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [authNumber, setAuthNumber] = useState("");
-  const [authNumberErrorMessage, setAuthNumberErrorMessage] = useState("");
-  const [phoneNumberErrorMessage, setPhoneNumberErrorMessage] = useState("");
-  const [authButtonText, setAuthButtonText] = useState<
-    "전송하기" | "재전송하기"
-  >("전송하기");
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [authNumber, setAuthNumber] = useState('');
+  const [authNumberErrorMessage, setAuthNumberErrorMessage] = useState('');
+  const [phoneNumberErrorMessage, setPhoneNumberErrorMessage] = useState('');
+  const [authButtonText, setAuthButtonText] = useState<'전송하기' | '재전송하기'>('전송하기');
   const [isActive, setIsActive] = useState(false);
 
   const navigate = useNavigate();
@@ -35,25 +31,23 @@ function AuthSection({ children, nextURL }: AuthSectionProps) {
   };
 
   const timerCallback = () => {
-    setAuthNumberErrorMessage(
-      "3분이 초과되었어요. 인증번호를 다시 요청해주세요."
-    );
+    setAuthNumberErrorMessage('3분이 초과되었어요. 인증번호를 다시 요청해주세요.');
     setIsActive(false);
   };
 
-  const { timeLeft, resetTime } = useTimer(isActive, timerCallback, 10);
+  const { timeLeft, resetTime } = useTimer(isActive, timerCallback);
 
   const handleSendAuthNumber = async () => {
-    if (phoneNumber === "") {
-      setPhoneNumberErrorMessage("전화번호를 확인해주세요.");
+    if (phoneNumber === '') {
+      setPhoneNumberErrorMessage('전화번호를 확인해주세요.');
     } else {
       try {
         await postAuthPhone(phoneNumber);
 
-        setAuthButtonText("재전송하기");
-        setAuthNumber("");
-        setAuthNumberErrorMessage("");
-        setPhoneNumberErrorMessage("");
+        setAuthButtonText('재전송하기');
+        setAuthNumber('');
+        setAuthNumberErrorMessage('');
+        setPhoneNumberErrorMessage('');
         resetTime();
         setIsActive(true);
       } catch (error) {
@@ -66,9 +60,13 @@ function AuthSection({ children, nextURL }: AuthSectionProps) {
 
   const handleAuthComplete = async () => {
     try {
-      await postVerifyPhone(phoneNumber, authNumber);
+      const response = await postVerifyPhone(phoneNumber, authNumber);
+      const { name, phone } = response.data;
 
-      setAuthNumberErrorMessage("");
+      sessionStorage.setItem('name', name);
+      sessionStorage.setItem('phone', phone);
+
+      setAuthNumberErrorMessage('');
       navigate({ to: nextURL });
     } catch (error) {
       if (error instanceof Error) {
@@ -90,10 +88,7 @@ function AuthSection({ children, nextURL }: AuthSectionProps) {
             errorMessage={phoneNumberErrorMessage}
             className={css({ ...phoneInputStyles })}
           />
-          <Button
-            className={css({ ...sendAuthNumberButtonStyles })}
-            onClick={handleSendAuthNumber}
-          >
+          <Button className={css({ ...sendAuthNumberButtonStyles })} onClick={handleSendAuthNumber}>
             {authButtonText}
           </Button>
         </div>
@@ -106,27 +101,17 @@ function AuthSection({ children, nextURL }: AuthSectionProps) {
             errorMessage={authNumberErrorMessage}
             className={css({ ...authNumberInputStyles })}
           />
-          <span
-            className={cx(
-              timeStyles,
-              authNumberErrorMessage.length > 0 && errorTextStyles
-            )}
-          >
-            {timeLeft === 180 ? "3:00" : formatTime(timeLeft)}
+          <span className={cx(timeStyles, authNumberErrorMessage.length > 0 && errorTextStyles)}>
+            {timeLeft === 180 ? '3:00' : formatTime(timeLeft)}
           </span>
         </div>
       </section>
       {children}
       <Button
         size="lg"
-        disabled={
-          !isActive ||
-          authNumber.length === 0 ||
-          authNumberErrorMessage.length > 0
-        }
+        disabled={!isActive || authNumber.length === 0 || authNumberErrorMessage.length > 0}
         onClick={handleAuthComplete}
-        className={css({ ...completeButtonStyles })}
-      >
+        className={css({ ...completeButtonStyles })}>
         SOPT 회원인증 완료
       </Button>
     </>
@@ -136,32 +121,32 @@ function AuthSection({ children, nextURL }: AuthSectionProps) {
 export default AuthSection;
 
 const authSectionStyles = css.raw({
-  display: "flex",
-  flexDirection: "column",
-  gap: "1.8rem",
-  marginTop: "4.8rem",
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.8rem',
+  marginTop: '4.8rem',
 
-  "@media (max-width: 480px)": {
-    gap: "1.4rem",
+  '@media (max-width: 480px)': {
+    gap: '1.4rem',
   },
 });
 
 const phoneTitleStyles = css.raw({
-  textStyle: "label-2-16-sb",
-  color: "gray.400",
+  textStyle: 'label-2-16-sb',
+  color: 'gray.400',
 
-  "@media (max-width: 480px)": {
-    textStyle: "label-3-14-sb",
+  '@media (max-width: 480px)': {
+    textStyle: 'label-3-14-sb',
   },
 });
 
 const phoneWrapperStyles = css.raw({
-  display: "flex",
-  alignItems: "baseline",
-  columnGap: "1.2rem",
+  display: 'flex',
+  alignItems: 'baseline',
+  columnGap: '1.2rem',
 
-  "@media (max-width: 480px)": {
-    gap: "0.7rem",
+  '@media (max-width: 480px)': {
+    gap: '0.7rem',
   },
 });
 
@@ -170,49 +155,49 @@ const phoneInputStyles = css.raw({
 });
 
 const authNumberInputStyles = css.raw({
-  width: "42rem",
-  height: "48px",
+  width: '42rem',
+  height: '48px',
 
-  "@media (max-width: 480px)": {
-    width: "100%",
+  '@media (max-width: 480px)': {
+    width: '100%',
   },
 });
 
 const timeStyles = css({
-  position: "absolute",
-  right: "1.6rem",
-  textStyle: "body-2-16-m",
-  color: "white",
+  position: 'absolute',
+  right: '1.6rem',
+  textStyle: 'body-2-16-m',
+  color: 'white',
 
-  "@media (max-width: 480px)": {
-    textStyle: "body-3-14-m",
+  '@media (max-width: 480px)': {
+    textStyle: 'body-3-14-m',
   },
 });
 
 const errorTextStyles = css({
-  color: "red.400",
+  color: 'red.400',
 });
 
 const authNumberWrapperStyles = css.raw({
-  position: "relative",
-  display: "flex",
-  alignItems: "center",
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
 });
 
 const completeButtonStyles = css.raw({
-  marginTop: "4.8rem",
-  width: "100%",
+  marginTop: '4.8rem',
+  width: '100%',
 
-  "@media (max-width: 480px)": {
-    marginTop: "auto",
-    marginBottom: "3.4rem",
+  '@media (max-width: 480px)': {
+    marginTop: 'auto',
+    marginBottom: '3.4rem',
   },
 });
 
 const sendAuthNumberButtonStyles = css.raw({
-  width: "11.6rem",
+  width: '11.6rem',
 
-  "@media (max-width: 480px)": {
-    width: "11rem",
+  '@media (max-width: 480px)': {
+    width: '11rem',
   },
 });
