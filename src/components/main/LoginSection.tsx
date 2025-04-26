b10ec1c320cbf19c2a36ed778e23cdb32859a029
wuzoo <wuzoo@daangn.com>
2025-04-26 20:35:18 +0900

    feat: dev 서버 테스트 용 핸들러 작성

--- src/components/main/LoginSection.tsx
+++ src/components/main/LoginSection.tsx
@@ -1,14 +1,15 @@
-"use client";
+'use client';
 
-import { css } from "@/styled-system/css";
+import { css } from '@/styled-system/css';
 
-import LoginButton from "./LoginButton";
-import { IconChevronRight } from "@sopt-makers/icons";
-import LastLoggedInBanner from "./LastLoggedInBanner";
-import CannotLoginModal from "@/src/components/common/CannotLoginModal";
-import { useState } from "react";
-import { Link } from "@tanstack/react-router";
-import { getGoogleAuthUrl } from "@/src/utils/google";
+import LoginButton from './LoginButton';
+import { IconChevronRight } from '@sopt-makers/icons';
+import LastLoggedInBanner from './LastLoggedInBanner';
+import CannotLoginModal from '@/src/components/common/CannotLoginModal';
+import { useState } from 'react';
+import { Link } from '@tanstack/react-router';
+import { getGoogleAuthUrl } from '@/src/utils/google';
+import { generateNonce } from '@/src/utils/nonce';
 
 function LoginSection() {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -22,11 +23,30 @@ function LoginSection() {
   };
 
   const handleGoogleLogin = () => {
-    location.href = getGoogleAuthUrl({ state: "login" });
+    location.href = getGoogleAuthUrl({ state: 'login' });
   };
 
-  // TODO: Apple Login 구현
-  // const handleAppleLogin = () => {};
+  const handleAppleLogin = async () => {
+    window.AppleID.auth.init({
+      clientId: 'com.auth-frontend.sopt',
+      scope: 'name email',
+      redirectURI: 'https://sopt-auth-frontend.pages.dev/auth/apple/callback',
+      state: 'login',
+      nonce: generateNonce(30),
+      usePopup: true,
+    });
+
+    const nonce = generateNonce(30);
+    sessionStorage.setItem('nonce', nonce);
+
+    try {
+      const response = await window.AppleID.auth.signIn();
+
+      console.log(response);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <>
@@ -40,17 +60,13 @@ function LoginSection() {
             buttonIcon={<img src="/google.svg" alt="구글 로고" />}
           />
           <LoginButton
+            onClick={handleAppleLogin}
             buttonText="Apple로 로그인"
             buttonIcon={<img src="/apple.svg" alt="애플 로고" />}
           />
         </section>
-        <button
-          className={css({ ...cannotLoginButtonStyles })}
-          onClick={handleClickCannotLoginButton}
-        >
-          <span className={css({ ...cannotLoginButtonTextStyles })}>
-            로그인이 안 되나요?
-          </span>
+        <button className={css({ ...cannotLoginButtonStyles })} onClick={handleClickCannotLoginButton}>
+          <span className={css({ ...cannotLoginButtonTextStyles })}>로그인이 안 되나요?</span>
           <IconChevronRight className={css({ ...iconChevronRightStyles })} />
         </button>
         <div className={css({ ...orWrapperStyles })}>
@@ -59,9 +75,7 @@ function LoginSection() {
           <div className={css({ ...orLineStyles })} />
         </div>
         <Link to="/sign-up/auth">
-          <button className={css({ ...signUpButtonStyles })}>
-            SOPT 회원가입
-          </button>
+          <button className={css({ ...signUpButtonStyles })}>SOPT 회원가입</button>
         </Link>
       </div>
     </>
@@ -71,86 +85,86 @@ function LoginSection() {
 export default LoginSection;
 
 const loginSectionStyles = css.raw({
-  marginTop: "3.5rem",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
+  marginTop: '3.5rem',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 
-  "@media (max-width: 480px)": {
-    marginTop: "3.2rem",
+  '@media (max-width: 480px)': {
+    marginTop: '3.2rem',
   },
 });
 
 const loginButtonSectionStyles = css.raw({
-  display: "flex",
-  flexDirection: "column",
-  gap: "1.2rem",
-  marginTop: "0.8rem",
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.2rem',
+  marginTop: '0.8rem',
 });
 
 const cannotLoginButtonStyles = css.raw({
-  display: "flex",
-  alignItems: "center",
-  marginTop: "1.9rem",
-  color: "gray.30",
+  display: 'flex',
+  alignItems: 'center',
+  marginTop: '1.9rem',
+  color: 'gray.30',
 
-  "&:hover": {
-    cursor: "pointer",
-    color: "gray.50",
+  '&:hover': {
+    cursor: 'pointer',
+    color: 'gray.50',
   },
 
-  "&:active": {
-    color: "gray.100",
+  '&:active': {
+    color: 'gray.100',
   },
 });
 
 const cannotLoginButtonTextStyles = css.raw({
-  textStyle: "label-3-14-sb",
+  textStyle: 'label-3-14-sb',
 });
 
 const iconChevronRightStyles = css.raw({
-  width: "1.6rem",
-  height: "1.6rem",
+  width: '1.6rem',
+  height: '1.6rem',
 });
 
 const orWrapperStyles = css.raw({
-  display: "flex",
-  gap: "0.8rem",
-  alignItems: "center",
-  marginTop: "2.4rem",
+  display: 'flex',
+  gap: '0.8rem',
+  alignItems: 'center',
+  marginTop: '2.4rem',
 });
 const orLineStyles = css.raw({
-  width: "14.05rem",
-  height: "0.1rem",
-  backgroundColor: "gray.300",
+  width: '14.05rem',
+  height: '0.1rem',
+  backgroundColor: 'gray.300',
 });
 
 const orTextStyles = css.raw({
-  textStyle: "body-4-13-l",
-  color: "gray.300",
+  textStyle: 'body-4-13-l',
+  color: 'gray.300',
 });
 
 const signUpButtonStyles = css.raw({
-  width: "38rem",
-  height: "5.6rem",
-  marginTop: "1.6rem",
-  borderRadius: "1.2rem",
-  textStyle: "label-1-18-sb",
-  color: "white",
-  backgroundColor: "gray.700",
+  width: '38rem',
+  height: '5.6rem',
+  marginTop: '1.6rem',
+  borderRadius: '1.2rem',
+  textStyle: 'label-1-18-sb',
+  color: 'white',
+  backgroundColor: 'gray.700',
 
-  "&:hover": {
-    cursor: "pointer",
-    backgroundColor: "gray.800",
+  '&:hover': {
+    cursor: 'pointer',
+    backgroundColor: 'gray.800',
   },
 
-  "&:active": {
-    backgroundColor: "gray.900",
+  '&:active': {
+    backgroundColor: 'gray.900',
   },
 
-  "@media (max-width: 480px)": {
-    width: "33.5rem",
-    height: "4.8rem",
-    textStyle: "label-2-16-sb",
+  '@media (max-width: 480px)': {
+    width: '33.5rem',
+    height: '4.8rem',
+    textStyle: 'label-2-16-sb',
   },
 });
