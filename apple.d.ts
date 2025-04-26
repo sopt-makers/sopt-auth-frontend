952dabe9a8d117e37f941b8d676083bfb9ebacc9
wuzoo <wuzoo@daangn.com>
2025-04-26 20:34:58 +0900

    feat: window apple 프로퍼티 설정

---
+++ apple.d.ts
@@ -0,0 +1,37 @@
+export {};
+
+interface ClientConfig {
+  clientId: string;
+  redirectURI: string;
+  scope?: string;
+  state?: string;
+  nonce?: string;
+  usePopup?: boolean;
+}
+
+interface Authorization {
+  code: string;
+  id_token: string;
+  state?: string;
+}
+
+interface User {
+  email: string;
+  name: string;
+}
+
+interface SigninResponse {
+  authorization: Authorization;
+  user?: User;
+}
+
+declare global {
+  interface Window {
+    AppleID: {
+      auth: {
+        init: (config: ClientConfig) => void;
+        signIn: (config?: ClientConfig) => Promise<SigninResponse>;
+      };
+    };
+  }
+}
