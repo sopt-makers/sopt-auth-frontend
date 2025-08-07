a7c46b09a95e17be03c6bd3e7fbd01c6806e3275
10tentacion <ddww1029wwdd@gmail.com>
2025-08-07 22:11:31 +0900

    feat: vite config base 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [TanStackRouterVite(), react()],
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "./") }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './') }],
   },
-});
+  base: mode === 'production' ? '/accounts' : '/',
+}));
