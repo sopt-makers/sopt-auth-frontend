9d8ebc4833b9593a1ef326b1d51b9f1c67052e9c
10tacion <127329855+10tacion@users.noreply.github.com>
2025-08-19 23:48:53 +0900

    feat: 리다이렉트 로직 추가 (#43)

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -11,9 +11,20 @@ export const useLogin = () => {
       localStorage.setItem('serviceAccessToken', response.data.accessToken);
       // NOTE: 기존 pg-lastRegister를 sopt-lastRegister로 변경
       localStorage.setItem('sopt-lastRegister', authPlatform === 'GOOGLE' ? 'google' : 'apple');
+      const lastUnauthorizedPath = sessionStorage.getItem('lastUnauthorizedPath');
 
       // NOTE: 서버 테스트용 복사 프롬프트 주석처리
       // prompt('서버테스트용 액세스 토큰 추출 (Ctrl+C로 복사하세요):', response.data.accessToken);
+
+      if (response.data?.isFirstLogin === true) {
+        window.location.href = `${VITE_SUCCESS_CALLBACK}/members/edit`;
+        return;
+      }
+
+      if (lastUnauthorizedPath) {
+        window.location.href = lastUnauthorizedPath;
+        return;
+      }
 
       window.location.href = VITE_SUCCESS_CALLBACK;
     } catch (error) {
